cd079befea3ff9e1057b20037da95d0a6501b5d1
Pila-night <dbnz2005@list.ru>
2024-10-16 00:22:29 +0300

    Добавлена обработка отрицательных чисел и чисел в экспоненциальной форме

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,109 +1,118 @@
 #include "Calc.h"
-#include <sstream>
+
 #include <cstdlib>
 #include <getopt.h>
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
-
+#include <regex>
+#include <regex>
 using namespace std;
 
-void print_help() {
+void print_help()
+{
     cout << "=============================\n";
     cout << "          Калькулятор        \n";
     cout << "=============================\n";
-    cout << "Использование: ./Calc_m [-o <операция>] <числа>\n\n";
-    
+    cout << "Использование: ./Calculator <-o <операция>> <числа>\n\n";
+
     cout << "Опции:\n";
     cout << "   -h, --help     - Показать справку\n\n";
-    
+
     cout << "Операции:\n";
     cout << "   m, multiply  - Умножение\n";
     cout << "   d, divide    - Деление\n\n";
 
     cout << "Примечание:\n";
     cout << "   Для выполнения операций необходимо указать от 3 до 5 чисел.\n";
-    cout << "   Пример: ./Calculator -o m 2 3 4\n";
-    
+    cout << "   Пример: ./Calculator -o m 2 3 4\n\n ";
+    cout << "  Калькулятор умеет работать с числами в экспоненциальной форме.\n";
+    cout << "   Пример: ./Calculator -o m 1.45e1 -1.22e-5 1\n";
+    cout << "   Внимание! После 'e' должна быть обязательно указана степень.\n";
     cout << "=============================\n";
 }
 
+
 bool is_number(const string& s) {
-    istringstream iss(s);
-    double dummy;
-    return !(iss >> dummy).fail();
+    const regex pattern(R"([-+]?[0-9]*.?[0-9]+([eE][-+]?[0-9]+)?)");
+    return regex_match(s, pattern);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
+
+
+bool is_option(const string& s)
+{
+    if(is_number(s)) {
+        return false; 
+    }
+    return s == "-o" || s == "--help" || s == "m" || s == "multiply" || s == "d" || s == "divide";
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3) {
         print_help();
         return 1;
     }
 
     Calc calculator;
-    int opt;
     string operation;
-
-    while ((opt = getopt(argc, argv, "o:hd")) != -1) {
-        switch (opt) {
-            case 'h':
-                print_help();
-                return 0;
-            case 'o':
-                operation = string(optarg);
-                break;
-            default:
+    vector<double> operands;
+    bool operation_found = false;
+    for(int i = 1; i < argc; ++i) {
+        if(string(argv[i]) == "-o" && (i + 1) < argc) {
+            operation = argv[++i]; 
+            if(operation != "m" && operation != "multiply" && operation != "d" && operation != "divide") {
+                cerr << "Ошибка: Неверная операция: '" << operation << "'.\n";
                 print_help();
                 return 1;
-        }
-    }
-
-    if (operation.empty()) {
-        cerr << "Ошибка: не указан тип операции." << endl;
-        print_help();
-        return 1; 
-    }
-
-    if (operation != "m" && operation != "multiply" && operation != "d" && operation != "divide") {
-        cerr << "Ошибка: неверный тип операции. Доступны операции: 'm' (multiply), 'd' (divide)." << endl;
-        print_help();
-        return 1;
-    }
-
-    vector<double> operands;
-
-    for (int i = optind; i < argc; ++i) {
-        if (is_number(argv[i])) {
-            operands.push_back(stod(argv[i]));
+            }
+            operation_found = true; 
+        } else if(is_option(argv[i])) {
+            continue;
+        } else if(is_number(argv[i])) {
+            if(operation_found) { 
+                operands.push_back(stod(argv[i])); 
+            } else {
+                cerr << "Ошибка: числа должны стоять после опции '-o' и типа операции." << endl;
+                return 1;
+            }
         } else {
-            cerr << "Ошибка: '" << argv[i] << "' не является числом." << endl;
+            cerr << "Ошибка: '" << argv[i] << "' является неизвестной опцией." << endl;
             return 1;
         }
     }
 
-    if (operands.size() < 3 || operands.size() > 5) {
+    if(operation.empty()) {
+        cerr << "Ошибка: Не указана операция." << endl;
+        print_help();
+        return 1;
+    }
+
+    if(operands.size() < 3 || operands.size() > 5) {
         cerr << "Ошибка: для работы калькулятора необходимо ввести от 3 до 5 значений." << endl;
         return 1;
     }
 
-    try {
-        double result;
-        if (operation == "m" || operation == "multiply") {
-            result = calculator.multiply(operands);
-            cout << "=============================\n";
-            cout << "Результат умножения: " << result << "\n";
-            cout << "=============================\n";
-        } else if (operation == "d" || operation == "divide") {
-            result = calculator.divide(operands);
+    if(operation == "m" || operation == "multiply") {
+        double result = calculator.multiply(operands);
+        cout << "=============================\n";
+        cout << "Результат умножения: " << result << "\n";
+        cout << "=============================\n";
+    } else if(operation == "d" || operation == "divide") {
+        try {
+            double result = calculator.divide(operands);
             cout << "=============================\n";
             cout << "Результат деления: " << result << "\n";
             cout << "=============================\n";
-        } else {
-            throw invalid_argument("Ошибка: не выбрана операция.");
+        } catch(const exception& e) {
+            cerr << "Ошибка: " << e.what() << endl;
+            return 1;
         }
-    } catch (const exception& e) {
-        cerr << "Ошибка: " << e.what() << endl;
+    } else {
+        print_help();
         return 1;
     }
 
